0f2e5aec053308ef762b2bb92936c1a17a7e22c7
rastuak <rastuakfaisal@gmail.com>
2024-06-28 03:34:31 +0700

    changes User.tsx

--- app/components/User.tsx
+++ app/components/User.tsx
@@ -1,236 +1,222 @@
-<<<<<<< HEAD
-import React from "react";
-import { View, Text, TouchableOpacity, Image } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+  StyleSheet,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-
-const User = ( {navigation} : {navigation: any} ) => {
-  const floor = "1";
-  return (
-    <SafeAreaView className="h-full bg-blackgray px-8 py-5">
-        <TouchableOpacity onPress={() => navigation.navigate("SelectionPage")}>
-          {/* <View></View> */}
-          <Image
-            source={require("./../../assets/arrow-left.png")}
-            // className="w-10 h-10"
-          />
-        </TouchableOpacity>
-
-        <Text className="text-5xl font-black text-white mt-8">
-          Floor : {floor}{" "}
-        </Text>
-
-      <Text className="text-2xl text-white font-semibold mt-14">
-        Your Position:
-      </Text>
-    </SafeAreaView>
-  );
-=======
-import React, {useState, useEffect} from "react";
-import {Text, View, Dimensions, Image, StyleSheet} from "react-native";
-import {SafeAreaView} from "react-native-safe-area-context";
-import Svg, {Rect, Circle} from "react-native-svg";
+import React, { useState, useEffect } from "react";
 import useBLE from "../../hooks/useBLE";
 import Geolocation from "react-native-geolocation-service";
 import map2 from "./../../assets/map-pusgiwa.png";
 import map1 from "./../../assets/map-pusgiwa.png";
 
-const User = () => {
-	const {
-		scanForPeripherals,
-		scanForClassicDevices,
-		requestPermissions,
-		allDevices,
-	} = useBLE();
-	const [floor, setFloor] = useState(0);
 
-	const deviceNames = [
-		["1A", "1B"],
-		["2A", "2B"],
-	];
+const User = ( {navigation}: {navigation: any} ) => {
+  const {
+    scanForPeripherals,
+    scanForClassicDevices,
+    requestPermissions,
+    allDevices,
+  } = useBLE();
+  const [floor, setFloor] = useState(0);
 
-	const floorPrediction = () => {
-		var rssiTotal = new Array(deviceNames.length).fill(0);
-		for (let i = 0; i < allDevices.length; i++) {
-			for (let j = 0; j < deviceNames.length; j++) {
-				if (
-					allDevices[i].name === deviceNames[j][0] ||
-					allDevices[i].name === deviceNames[j][1]
-				) {
-					rssiTotal[j] -= 1 / Number(allDevices[i].rssi);
-				}
-			}
-		}
-		console.log(rssiTotal);
-		setFloor(rssiTotal.indexOf(Math.max(...rssiTotal)) + 1);
-	};
+  const deviceNames = [
+    ["1A", "1B"],
+    ["2A", "2B"],
+  ];
 
-	const [location, setLocation] = useState({latitude: 0, longitude: 0});
-	const [watchId, setWatchId] = useState(0);
+  const floorPrediction = () => {
+    var rssiTotal = new Array(deviceNames.length).fill(0);
+    for (let i = 0; i < allDevices.length; i++) {
+      for (let j = 0; j < deviceNames.length; j++) {
+        if (
+          allDevices[i].name === deviceNames[j][0] ||
+          allDevices[i].name === deviceNames[j][1]
+        ) {
+          rssiTotal[j] -= 1 / Number(allDevices[i].rssi);
+        }
+      }
+    }
+    console.log(rssiTotal);
+    setFloor(rssiTotal.indexOf(Math.max(...rssiTotal)) + 1);
+  };
 
-	const getLocation = () => {
-		console.log("getting location");
-		const id = Geolocation.watchPosition(
-			(position) => {
-				setLocation({
-					latitude: position.coords.latitude,
-					longitude: position.coords.longitude,
-				});
-				console.log("Location: ", position);
-			},
-			(error) => {
-				console.log(error.code, error.message);
-			},
-			{
-				enableHighAccuracy: true,
-				interval: 1000,
-				distanceFilter: 2,
-				fastestInterval: 1000,
-			}
-		);
+  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+  const [watchId, setWatchId] = useState(0);
 
-		console.log(id);
-		setWatchId(id);
-	};
+  const getLocation = () => {
+    console.log("getting location");
+    const id = Geolocation.watchPosition(
+      (position) => {
+        setLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        console.log("Location: ", position);
+      },
+      (error) => {
+        console.log(error.code, error.message);
+      },
+      {
+        enableHighAccuracy: true,
+        interval: 1000,
+        distanceFilter: 2,
+        fastestInterval: 1000,
+      }
+    );
 
-	useEffect(() => {
-		const startBLE = async () => {
-			const hasPermissions = await requestPermissions();
-			if (hasPermissions) {
-				scanForPeripherals();
-				scanForClassicDevices();
-				floorPrediction();
-			} else {
-				console.log("Permissions not granted");
-			}
-		};
-		startBLE();
-	}, [allDevices]);
+    console.log(id);
+    setWatchId(id);
+  };
 
-	useEffect(() => {
-		const getPermissions = async () => {
-			const hasPermissions = await requestPermissions();
-			if (hasPermissions) {
-				getLocation();
-			} else {
-				console.log("Permissions not granted");
-			}
-		};
+  useEffect(() => {
+    const startBLE = async () => {
+      const hasPermissions = await requestPermissions();
+      if (hasPermissions) {
+        scanForPeripherals();
+        scanForClassicDevices();
+        floorPrediction();
+      } else {
+        console.log("Permissions not granted");
+      }
+    };
+    startBLE();
+  }, [allDevices]);
 
-		getPermissions();
+  useEffect(() => {
+    const getPermissions = async () => {
+      const hasPermissions = await requestPermissions();
+      if (hasPermissions) {
+        getLocation();
+      } else {
+        console.log("Permissions not granted");
+      }
+    };
 
-		return () => {
-			Geolocation.clearWatch(watchId);
-		};
-	}, []);
+    getPermissions();
 
-	const getPositionInBuilding = (latitude: number, longitude: number) => {
-		const buildingCoords = {
-			topLeft: {
-				latitude: -6.365057675942874,
-				longitude: 106.82418537481664,
-			},
-			topRight: {
-				latitude: -6.365064093655577,
-				longitude: 106.82472605124961,
-			},
-			bottomLeft: {
-				latitude: -6.365660806695966,
-				longitude: 106.8241676932336,
-			},
-			bottomRight: {
-				latitude: -6.365675619543003,
-				longitude: 106.82470571011808,
-			},
-		};
+    return () => {
+      Geolocation.clearWatch(watchId);
+    };
+  }, []);
 
-		const buildingWidth =
-			buildingCoords.topRight.longitude -
-			buildingCoords.topLeft.longitude;
-		const buildingHeight =
-			buildingCoords.topLeft.latitude -
-			buildingCoords.bottomLeft.latitude;
+  const getPositionInBuilding = (latitude: number, longitude: number) => {
+    const buildingCoords = {
+      topLeft: {
+        latitude: -6.365057675942874,
+        longitude: 106.82418537481664,
+      },
+      topRight: {
+        latitude: -6.365064093655577,
+        longitude: 106.82472605124961,
+      },
+      bottomLeft: {
+        latitude: -6.365660806695966,
+        longitude: 106.8241676932336,
+      },
+      bottomRight: {
+        latitude: -6.365675619543003,
+        longitude: 106.82470571011808,
+      },
+    };
 
-		const relativeX =
-			((longitude - buildingCoords.topLeft.longitude) / buildingWidth) *
-			100;
-		const relativeY =
-			((buildingCoords.topLeft.latitude - latitude) / buildingHeight) *
-			100;
+    const buildingWidth =
+      buildingCoords.topRight.longitude - buildingCoords.topLeft.longitude;
+    const buildingHeight =
+      buildingCoords.topLeft.latitude - buildingCoords.bottomLeft.latitude;
 
-		console.log(relativeX, relativeY);
-		return {x: relativeX, y: relativeY};
-	};
+    const relativeX =
+      ((longitude - buildingCoords.topLeft.longitude) / buildingWidth) * 100;
+    const relativeY =
+      ((buildingCoords.topLeft.latitude - latitude) / buildingHeight) * 100;
 
-	const userPosition = getPositionInBuilding(
-		location.latitude,
-		location.longitude
-	);
+    console.log(relativeX, relativeY);
+    return { x: relativeX, y: relativeY };
+  };
 
-	const map = [map1, map2];
+  const userPosition = getPositionInBuilding(
+    location.latitude,
+    location.longitude
+  );
 
-	return (
-		<SafeAreaView>
-			<Text style={styles.title}>Floor: {floor}</Text>
-			<Text style={styles.subHeading}>Your Position: </Text>
-			<Text>
-				location: {location.latitude}, {location.longitude}
-			</Text>
-			<View style={styles.container}>
-				<Image
-					source={floor != 0 ? map[floor - 1] : map[0]}
-					style={styles.map}
-					resizeMode="contain"
-				/>
-				<View
-					style={[
-						styles.marker,
-						{
-							left: `${userPosition.x}%`,
-							top: `${userPosition.y}%`,
-						},
-					]}
-				/>
-			</View>
-		</SafeAreaView>
-	);
->>>>>>> origin/main
+  const map = [map1, map2];
+
+  return (
+    <SafeAreaView className="h-full bg-blackgray px-8 py-5">
+      <TouchableOpacity onPress={() => navigation.navigate("SelectionPage")}>
+        <Image source={require("./../../assets/arrow-left.png")} />
+      </TouchableOpacity>
+
+      <Text className="text-5xl font-black text-white mt-8">
+        Floor : {floor}
+      </Text>
+
+      <Text className="text-2xl text-white font-semibold mt-14">
+        Your Position:
+      </Text>
+
+      <Text>
+        location: {location.latitude}, {location.longitude}
+      </Text>
+
+      <View style={styles.container}>
+        <Image
+          source={floor != 0 ? map[floor - 1] : map[0]}
+          style={styles.map}
+          resizeMode="contain"
+        />
+        <View
+          style={[
+            styles.marker,
+            {
+              left: `${userPosition.x}%`,
+              top: `${userPosition.y}%`,
+            },
+          ]}
+        />
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-	title: {
-		fontSize: 40,
-		fontWeight: "bold",
-		marginLeft: 32,
-		textAlign: "left",
-		marginBottom: 16,
-	},
-	subHeading: {
-		fontSize: 32,
-		fontWeight: "bold",
-		marginTop: 32,
-		marginLeft: 32,
-		textAlign: "left",
-		marginBottom: 16,
-	},
-	container: {
-		padding: 0,
-		backgroundColor: "#38b6ff",
-		margin: 10,
-		width: Dimensions.get("window").width - 20,
-		height: (Dimensions.get("window").width - 20) * (46 / 40.67),
-		position: "relative",
-	},
-	map: {
-		width: "100%",
-		height: "100%",
-	},
-	marker: {
-		position: "absolute",
-		width: 10,
-		height: 10,
-		borderRadius: 5,
-		backgroundColor: "red",
-	},
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginLeft: 32,
+    textAlign: "left",
+    marginBottom: 16,
+  },
+  subHeading: {
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 32,
+    marginLeft: 32,
+    textAlign: "left",
+    marginBottom: 16,
+  },
+  container: {
+    padding: 0,
+    backgroundColor: "#38b6ff",
+    margin: 10,
+    width: Dimensions.get("window").width - 20,
+    height: (Dimensions.get("window").width - 20) * (46 / 40.67),
+    position: "relative",
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+  marker: {
+    position: "absolute",
+    width: 10,
+    height: 10,
+    borderRadius: 5,
+    backgroundColor: "red",
+  },
 });
 
 export default User;
