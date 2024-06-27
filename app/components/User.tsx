ef972d3aec7849049328f0cd92a714eaa151632a
Tianrider <hadiwijayachristian7@gmail.com>
2024-06-27 16:34:17 +0700

    pusgiwa, not finished

--- app/components/User.tsx
+++ app/components/User.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import {Text} from "react-native";
+import React, {useState, useEffect} from "react";
+import {Text, View, Dimensions, Image, StyleSheet} from "react-native";
 import {SafeAreaView} from "react-native-safe-area-context";
-import {useEffect, useState} from "react";
+import Svg, {Rect, Circle} from "react-native-svg";
 import useBLE from "../../hooks/useBLE";
 import Geolocation from "react-native-geolocation-service";
+import map2 from "./../../assets/map-pusgiwa.png";
+import map1 from "./../../assets/map-pusgiwa.png";
 
 const User = () => {
 	const {
@@ -20,7 +22,7 @@ const User = () => {
 	];
 
 	const floorPrediction = () => {
-		var rssiTotal: number[] = new Array(deviceNames.length).fill(0);
+		var rssiTotal = new Array(deviceNames.length).fill(0);
 		for (let i = 0; i < allDevices.length; i++) {
 			for (let j = 0; j < deviceNames.length; j++) {
 				if (
@@ -53,9 +55,9 @@ const User = () => {
 			},
 			{
 				enableHighAccuracy: true,
-				interval: 3000,
-				distanceFilter: 0,
-				fastestInterval: 500,
+				interval: 1000,
+				distanceFilter: 2,
+				fastestInterval: 1000,
 			}
 		);
 
@@ -94,6 +96,51 @@ const User = () => {
 		};
 	}, []);
 
+	const getPositionInBuilding = (latitude: number, longitude: number) => {
+		const buildingCoords = {
+			topLeft: {
+				latitude: -6.365057675942874,
+				longitude: 106.82418537481664,
+			},
+			topRight: {
+				latitude: -6.365064093655577,
+				longitude: 106.82472605124961,
+			},
+			bottomLeft: {
+				latitude: -6.365660806695966,
+				longitude: 106.8241676932336,
+			},
+			bottomRight: {
+				latitude: -6.365675619543003,
+				longitude: 106.82470571011808,
+			},
+		};
+
+		const buildingWidth =
+			buildingCoords.topRight.longitude -
+			buildingCoords.topLeft.longitude;
+		const buildingHeight =
+			buildingCoords.topLeft.latitude -
+			buildingCoords.bottomLeft.latitude;
+
+		const relativeX =
+			((longitude - buildingCoords.topLeft.longitude) / buildingWidth) *
+			100;
+		const relativeY =
+			((buildingCoords.topLeft.latitude - latitude) / buildingHeight) *
+			100;
+
+		console.log(relativeX, relativeY);
+		return {x: relativeX, y: relativeY};
+	};
+
+	const userPosition = getPositionInBuilding(
+		location.latitude,
+		location.longitude
+	);
+
+	const map = [map1, map2];
+
 	return (
 		<SafeAreaView>
 			<Text style={styles.title}>Floor: {floor}</Text>
@@ -101,16 +148,32 @@ const User = () => {
 			<Text>
 				location: {location.latitude}, {location.longitude}
 			</Text>
+			<View style={styles.container}>
+				<Image
+					source={floor != 0 ? map[floor - 1] : map[0]}
+					style={styles.map}
+					resizeMode="contain"
+				/>
+				<View
+					style={[
+						styles.marker,
+						{
+							left: `${userPosition.x}%`,
+							top: `${userPosition.y}%`,
+						},
+					]}
+				/>
+			</View>
 		</SafeAreaView>
 	);
 };
 
-const styles = {
+const styles = StyleSheet.create({
 	title: {
 		fontSize: 40,
 		fontWeight: "bold",
 		marginLeft: 32,
-		textAlign: "start",
+		textAlign: "left",
 		marginBottom: 16,
 	},
 	subHeading: {
@@ -118,9 +181,28 @@ const styles = {
 		fontWeight: "bold",
 		marginTop: 32,
 		marginLeft: 32,
-		textAlign: "start",
+		textAlign: "left",
 		marginBottom: 16,
 	},
-};
+	container: {
+		padding: 0,
+		backgroundColor: "#38b6ff",
+		margin: 10,
+		width: Dimensions.get("window").width - 20,
+		height: (Dimensions.get("window").width - 20) * (46 / 40.67),
+		position: "relative",
+	},
+	map: {
+		width: "100%",
+		height: "100%",
+	},
+	marker: {
+		position: "absolute",
+		width: 10,
+		height: 10,
+		borderRadius: 5,
+		backgroundColor: "red",
+	},
+});
 
 export default User;
